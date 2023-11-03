a753ff5e69b921e1c376c37dea7a1b51aa8480ac
kikos7 <chriseskaf@gmail.com>
2023-11-03 13:18:51 -0400

    added images of app

--- main.js
+++ main.js
@@ -70,3 +70,19 @@ function scrollToProjects() {
 // allCards.forEach((item) => {
 //   console.log(item.style.padding);
 // });
+
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    // document.body.style.backgroundColor = "yellow";
+    document.body.querySelector("#right-main-top").innerHTML =
+      "<h2>If you want to see my projects</h2>";
+  } else {
+    document.body.querySelector("#right-main-top").innerHTML =
+      "<h2>If you want to see my projects : </h2>";
+  }
+}
+
+var x = window.matchMedia("(max-width: 1340px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
